b17ad3a1890d96d506f9e0e1228a57f30c9f05db
Nerites <paula.s.ning@gmail.com>
2018-09-25 13:32:38 -0700

    Code cleanup in XRUtils per feedback

--- com.unity.render-pipelines.core/Runtime/Utilities/XRUtils.cs
+++ com.unity.render-pipelines.core/Runtime/Utilities/XRUtils.cs
@@ -4,20 +4,7 @@ namespace UnityEngine.Experimental.Rendering
 {
     public static class XRUtils
     {
-        public static void DrawOcclusionMesh(CommandBuffer cmd, Camera camera)
-        {   // This method expects XRGraphicsConfig.SetConfig() to have been called prior to it.
-            // Then, engine code will check if occlusion mesh rendering is enabled, and apply occlusion mesh scale.
-#if UNITY_2019_1_OR_NEWER
-            if ((!XRGraphicsConfig.enabled) || (!camera.stereoEnabled))
-                return;
-            UnityEngine.RectInt normalizedCamViewport = new UnityEngine.RectInt(0, 0, camera.pixelWidth, camera.pixelHeight);
-            cmd.DrawOcclusionMesh(normalizedCamViewport);
-#else
-            return;
-#endif
-        }
-
-        public static void DrawOcclusionMesh(CommandBuffer cmd, Camera camera, bool stereoEnabled) // This variant is for if the calling SRP has additional stereo suppression logic
+        public static void DrawOcclusionMesh(CommandBuffer cmd, Camera camera, bool stereoEnabled = true) // Optional stereoEnabled is for SRP-specific stereo logic
         {   // This method expects XRGraphicsConfig.SetConfig() to have been called prior to it.
             // Then, engine code will check if occlusion mesh rendering is enabled, and apply occlusion mesh scale.
 #if UNITY_2019_1_OR_NEWER
@@ -25,8 +12,6 @@ namespace UnityEngine.Experimental.Rendering
                 return;
             UnityEngine.RectInt normalizedCamViewport = new UnityEngine.RectInt(0, 0, camera.pixelWidth, camera.pixelHeight);
             cmd.DrawOcclusionMesh(normalizedCamViewport);
-#else
-            return;
 #endif
         }
 
